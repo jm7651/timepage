55598f2dd68517176ab369d600933e5ef44d75b0
Jungmin Park <65692480+jm7651@users.noreply.github.com>
2022-10-25 22:48:24 +0900

    agripa_003

--- project/ui/js/agripa_gsap.js
+++ project/ui/js/agripa_gsap.js
@@ -1,11 +1,12 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.to(".box01", {
+const tl = gsap.timeline({
   scrollTrigger: {
-    trigger: ".agripa_003",
+    trigger: ".agripa_002_desc",
+    start: "100 80%",
+    end: "+=300 50%%",
+    scrub: true,
     markers: true,
-    start: "top center",
-    end: "bottom bottom",
   },
-  height: 1000,
-  duration: 4,
 });
+tl.to(".box03", { height: "40%", duration: 1 });
+tl.to(".box02", { height: "70%", duration: 1 });
+tl.to(".box01", { height: "50%", duration: 1 });
