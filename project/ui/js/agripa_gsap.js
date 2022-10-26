df69692d770add0dc759019133c7e4dbf43cade1
jungminPark7651 <jm7651@naver.com>
2022-10-26 10:03:54 +0900

    agripa_004

--- project/ui/js/agripa_gsap.js
+++ project/ui/js/agripa_gsap.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".agripa_002_desc",
@@ -10,3 +11,39 @@ const tl = gsap.timeline({
 tl.to(".box03", { height: "40%", duration: 1 });
 tl.to(".box02", { height: "70%", duration: 1 });
 tl.to(".box01", { height: "50%", duration: 1 });
+
+var startCount = { var: 0 };
+var startCount1 = { var: 0 };
+var startCount2 = { var: 0 };
+gsap.to(startCount, {
+  var: 28,
+  duration: 2,
+  ease: "none",
+  onUpdate: changeNumber,
+  scrollTrigger: {
+    trigger: "#number0",
+  },
+});
+gsap.to(startCount1, {
+  var: 45,
+  duration: 2,
+  ease: "none",
+  onUpdate: changeNumber,
+  scrollTrigger: {
+    trigger: "#number1",
+  },
+});
+gsap.to(startCount2, {
+  var: 37,
+  duration: 2,
+  ease: "none",
+  onUpdate: changeNumber,
+  scrollTrigger: {
+    trigger: "#number2",
+  },
+});
+function changeNumber() {
+  number0.innerHTML = startCount.var.toFixed();
+  number1.innerHTML = startCount1.var.toFixed();
+  number2.innerHTML = startCount2.var.toFixed();
+}
