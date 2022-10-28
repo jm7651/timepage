401969ed12ed5860b110d0bf5469724c6ded1e20
jungminPark7651 <jm7651@naver.com>
2022-10-28 15:23:32 +0900

    agripa005

---
+++ project/ui/js/agripa_swiper.js
@@ -0,0 +1,30 @@
+function MySliderBox1__init() {
+  let swiper = new Swiper(".my-slider-box-1", {
+    slidesPerView: 1, //640~1024 해상도 외 레이아웃 뷰 개수
+    spaceBetween: 20,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }, //위 slidesPerview 여백/위 slidesPerview 여백
+    breakpoints: {
+      //반응형 조건 속성
+      640: {
+        //640 이상일 경우
+        slidesPerView: 2, //레이아웃 2열
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 5.5,
+      },
+    },
+  });
+}
+MySliderBox1__init();
