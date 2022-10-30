8a7ac87e1d4dca79e2c22749afd4f4997facef6e
Jungmin Park <65692480+jm7651@users.noreply.github.com>
2022-10-30 23:33:44 +0900

    agripa_005

--- project/ui/js/agripa_swiper.js
+++ project/ui/js/agripa_swiper.js
@@ -1,6 +1,6 @@
 function MySliderBox1__init() {
-  let swiper = new Swiper(".agripa_swiper01", {
-    slidesPerView: 1, //640~1024 해상도 외 레이아웃 뷰 개수
+  let swiper = new Swiper(".my-slider-box-1", {
+    slidesPerView: 1.5, //640~1024 해상도 외 레이아웃 뷰 개수
     spaceBetween: 20,
     autoplay: {
       delay: 2500,
@@ -8,19 +8,9 @@ function MySliderBox1__init() {
     },
     loop: true,
     loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    }, //위 slidesPerview 여백/위 slidesPerview 여백
+
     breakpoints: {
       //반응형 조건 속성
-      640: {
-        //640 이상일 경우
-        slidesPerView: 2, //레이아웃 2열
-      },
-      768: {
-        slidesPerView: 3,
-      },
       1024: {
         slidesPerView: 5.5,
       },
@@ -28,3 +18,25 @@ function MySliderBox1__init() {
   });
 }
 MySliderBox1__init();
+
+function MySliderBox2__init() {
+  let swiper = new Swiper(".my-slider-box-2", {
+    slidesPerView: 1, //640~1024 해상도 외 레이아웃 뷰 개수
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }, //위 slidesPerview 여백
+    breakpoints: {
+      //반응형 조건 속성
+      640: {
+        //640 이상일 경우
+        slidesPerView: 2, //레이아웃 2열
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+}
+MySliderBox2__init();
