18c2332f5a7ec41aa67890ae1dabbb3e36aee23e
jungminPark7651 <65692480+jm7651@users.noreply.github.com>
2022-11-10 16:32:20 +0900

    index_002

--- project/ui/js/hems_gsap.js
+++ project/ui/js/hems_gsap.js
@@ -1,33 +1,52 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.fromTo(
-  ".pain_point001",
-  { x: "-50%", rotate: "0" },
-  {
-    x: 0,
-    rotate: "360deg",
-    scrollTrigger: {
-      trigger: ".contents",
-      start: "100 80%",
-      end: "+=300 50%",
-      scrub: 1,
-      markers: true,
-      start: "top center",
-    },
-  }
-);
-gsap.fromTo(
-  ".pain_point002",
-  { x: "50%", rotate: "0" },
-  {
-    x: 0,
-    rotate: "-360deg",
-    scrollTrigger: {
-      trigger: ".contents",
-      start: "100 80%",
-      end: "+=300 50%",
-      scrub: 1,
-      // markers: true,
-      start: "top center",
-    },
-  }
-);
+ScrollTrigger.saveStyles(".mobile, .desktop");
+ScrollTrigger.matchMedia({
+  "(min-width: 800px)": function () {
+    gsap.fromTo(
+      ".pain_point001",
+      { x: "-50%", rotate: "0" },
+      {
+        x: 0,
+        rotate: "360deg",
+        scrollTrigger: {
+          trigger: ".contents",
+          start: "-100 50%",
+          end: "0 70%",
+          scrub: 1,
+          start: "top 80%",
+        },
+      }
+    );
+    gsap.fromTo(
+      ".pain_point002",
+      { y: "20rem" },
+      {
+        y: 0,
+        scrollTrigger: {
+          trigger: ".contents",
+          start: "-100 80%",
+          end: "0 50%",
+          scrub: 1,
+          // markers: true,
+          start: "top 80%",
+        },
+      }
+    );
+    gsap.fromTo(
+      ".pain_point003",
+      { x: "50%", rotate: "0" },
+      {
+        x: 0,
+        rotate: "-360deg",
+        scrollTrigger: {
+          trigger: ".contents",
+          start: "-100 80%",
+          end: "0 50%",
+          scrub: 1,
+          // markers: true,
+          start: "top 80%",
+        },
+      }
+    );
+  },
+});
