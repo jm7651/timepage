7f56e4a043cc2028eafe0050ac4bf0058c36be7d
jungminPark7651 <65692480+jm7651@users.noreply.github.com>
2022-11-16 17:01:59 +0900

    link

--- project/ui/js/summary_modal.js
+++ project/ui/js/summary_modal.js
@@ -1,8 +1,21 @@
 const modal = document.querySelector(".summary_modal");
 const btnModal = document.querySelector(".summary_button");
 const modalClose = window.parent.document.querySelector(".modal_close");
+const summaryClose = document.querySelector(".summary_close");
 btnModal.addEventListener("click", (e) => {
   modal.style.display = "flex";
   document.body.classList.add("stop-scroll");
   modalClose.style.display = "none";
+  console.log(summaryClose);
+});
+summaryClose.addEventListener("click", (e) => {
+  modal.style.display = "none";
+  document.body.classList.renove("stop-scroll");
+});
+modal.addEventListener("click", (e) => {
+  const evTarget = e.target;
+  if (evTarget.classList.contains("modal-overlay")) {
+    modal.style.display = "none";
+    document.body.classList.remove("stop-scroll");
+  }
 });
