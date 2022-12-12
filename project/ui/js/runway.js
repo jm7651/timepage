986353bf06db369123b50d19911586a0d32be4b6
jungminPark7651 <65692480+jm7651@users.noreply.github.com>
2022-12-12 16:50:29 +0900

    runway_001

--- project/ui/js/runway.js
+++ project/ui/js/runway.js
@@ -16,20 +16,35 @@ ScrollTrigger.matchMedia({
         },
       }
     );
-    const tl = gsap.timeline({
+    const tl001 = gsap.timeline({
       scrollTrigger: {
         trigger: ".runway_002",
         pin: true,
         scrub: 1,
         start: "top top",
-        end: "+=4000",
+        end: "+=2000",
         toggleActions: "play pause play reset",
       },
     });
-    tl.from(".runway_logo", {
+    tl001.from(".runway_logo", {
       opacity: 0,
       duration: 1,
     });
+    gsap.fromTo(
+      ".runway_003_main_img",
+      { y: "40rem" },
+      {
+        y: 0,
+        scrollTrigger: {
+          trigger: ".runway_003_main_img",
+          start: "-100 80%",
+          end: "0 70%",
+          scrub: 3,
+          // markers: true,
+        },
+      }
+    );
   },
+
   "(max-width: 799px)": function () {},
 });
