d8821d436445d442732f7056233c87cbd0dca81f
jungminPark7651 <65692480+jm7651@users.noreply.github.com>
2023-01-12 07:49:45 +0900

    index_001

--- project/ui/js/runway.js
+++ project/ui/js/runway.js
@@ -1,32 +1,57 @@
 gsap.registerPlugin(ScrollTrigger);
 ScrollTrigger.saveStyles(".mobile, .desktop");
+gsap.fromTo(
+  ".runway_002",
+  { opacity: 0 },
+  {
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".runway_002",
+      start: "500 80%",
+      end: "500 10%",
+      scrub: 1,
+      markers: true,
+    },
+  }
+);
+const tl001 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".runway_002",
+    pin: true,
+    scrub: 1,
+    start: "top top",
+    end: "+=2000",
+    toggleActions: "play pause play reset",
+  },
+});
+const tl002 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".runway_003",
+    pin: true,
+    scrub: 1,
+    start: "top top",
+    end: "+=2000",
+    toggleActions: "play pause play reset",
+  },
+});
+tl002.from(".runway_bg_title_01", {
+  opacity: 0,
+  duration: 1,
+});
+tl002.from(".runway_bg_title_02", {
+  opacity: 0,
+  duration: 1,
+});
+tl002.from(".runway_bg_title_03", {
+  opacity: 0,
+  duration: 1,
+});
+tl002.from(".runway_bg_title_04", {
+  opacity: 0,
+  duration: 1,
+});
 ScrollTrigger.matchMedia({
   "(min-width: 800px)": function () {
-    gsap.fromTo(
-      ".runway_002",
-      { opacity: 0 },
-      {
-        opacity: 1,
-        scrollTrigger: {
-          trigger: ".runway_002",
-          start: "500 80%",
-          end: "500 10%",
-          scrub: 1,
-          markers: true,
-        },
-      }
-    );
-    const tl001 = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".runway_002",
-        pin: true,
-        scrub: 1,
-        start: "top top",
-        end: "+=2000",
-        toggleActions: "play pause play reset",
-      },
-    });
-
     gsap.fromTo(
       ".runway_003_main_img",
       { y: "40rem" },
@@ -41,32 +66,7 @@ ScrollTrigger.matchMedia({
         },
       }
     );
-    const tl002 = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".runway_003",
-        pin: true,
-        scrub: 1,
-        start: "top top",
-        end: "+=2000",
-        toggleActions: "play pause play reset",
-      },
-    });
-    tl002.from(".runway_bg_title_01", {
-      opacity: 0,
-      duration: 1,
-    });
-    tl002.from(".runway_bg_title_02", {
-      opacity: 0,
-      duration: 1,
-    });
-    tl002.from(".runway_bg_title_03", {
-      opacity: 0,
-      duration: 1,
-    });
-    tl002.from(".runway_bg_title_04", {
-      opacity: 0,
-      duration: 1,
-    });
+
     gsap.fromTo(
       ".runway_loop video",
       { transform: "rotate3d(30, 1, 0, 50deg) scale(0.5)", opacity: "0" },
