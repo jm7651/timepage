206a9f61ff5c9998d3c797bc0e35f9449b8173b6
jungminPark7651 <65692480+jm7651@users.noreply.github.com>
2023-01-16 15:04:22 +0900

    runway_001

--- project/ui/js/summary_modal.js
+++ project/ui/js/summary_modal.js
@@ -1,22 +1,23 @@
 const modal = document.querySelector(".summary_modal");
 const btnModal = document.querySelector(".summary_button");
-const modalClose = window.parent.document.querySelector(".modal_close");
+// const modalClose = window.parent.document.querySelector(".modal_close");
 const summaryClose = document.querySelector(".summary_close");
 btnModal.addEventListener("click", (e) => {
+  console.log(modal);
   modal.style.display = "flex";
   document.body.classList.add("stop-scroll");
-  modalClose.style.display = "none";
+  // modalClose.style.display = "none";
 });
 summaryClose.addEventListener("click", (e) => {
   modal.style.display = "none";
   document.body.classList.remove("stop-scroll");
-  modalClose.style.display = "block";
+  // modalClose.style.display = "block";
 });
 modal.addEventListener("click", (e) => {
   const evTarget = e.target;
   if (evTarget.classList.contains("modal-overlay")) {
     modal.style.display = "none";
     document.body.classList.remove("stop-scroll");
-    modalClose.style.display = "block";
+    // modalClose.style.display = "block";
   }
 });
