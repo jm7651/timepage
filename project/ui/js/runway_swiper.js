d93782bba6abfecf276c9159a01c8ec77d835cd0
jungminPark7651 <65692480+jm7651@users.noreply.github.com>
2023-04-14 14:34:47 +0900

    commit

--- project/ui/js/runway_swiper.js
+++ project/ui/js/runway_swiper.js
@@ -1,6 +1,6 @@
 function MySliderBox2__init() {
   let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1, //640~1024 해상도 외 레이아웃 뷰 개수
+    slidesPerView: 1.3, //640~1024 해상도 외 레이아웃 뷰 개수
     spaceBetween: 20,
     pagination: {
       el: ".swiper-pagination",
@@ -10,7 +10,7 @@ function MySliderBox2__init() {
       //반응형 조건 속성
       640: {
         //640 이상일 경우
-        slidesPerView: 2, //레이아웃 2열
+        slidesPerView: 1.5, //레이아웃 2열
       },
       768: {
         slidesPerView: 3,
@@ -22,6 +22,7 @@ function MySliderBox2__init() {
   });
 }
 MySliderBox2__init();
+
 function MySliderBox3__init() {
   let swiper = new Swiper(".feature_concept", {
     slidesPerView: 3.5, //640~1024 해상도 외 레이아웃 뷰 개수
