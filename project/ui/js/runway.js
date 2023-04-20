1351dbe77df4d8d39f80850ad65c410f1a0bebaa
jungminPark7651 <65692480+jm7651@users.noreply.github.com>
2023-04-20 15:22:27 +0900

    commit

--- project/ui/js/runway.js
+++ project/ui/js/runway.js
@@ -1,46 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 ScrollTrigger.saveStyles(".mobile, .desktop");
 gsap.fromTo(
-  ".runway_003_main_img",
-  { y: "40rem" },
-  {
-    y: 0,
-    scrollTrigger: {
-      trigger: ".runway_003_main_img",
-      start: "-100 80%",
-      end: "0 70%",
-      scrub: 3,
-      // markers: true,
-    },
-  }
-);
-const tl002 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".runway_003",
-    pin: true,
-    scrub: 1,
-    start: "top top",
-    end: "+=2000",
-    toggleActions: "play pause play reset",
-  },
-});
-tl002.from(".runway_bg_title_01", {
-  opacity: 0,
-  duration: 1,
-});
-tl002.from(".runway_bg_title_02", {
-  opacity: 0,
-  duration: 1,
-});
-tl002.from(".runway_bg_title_03", {
-  opacity: 0,
-  duration: 1,
-});
-tl002.from(".runway_bg_title_04", {
-  opacity: 0,
-  duration: 1,
-});
-gsap.fromTo(
   ".runway_002",
   { opacity: 0 },
   {
@@ -63,21 +23,6 @@ const tl001 = gsap.timeline({
     toggleActions: "play pause play reset",
   },
 });
-gsap.fromTo(
-  ".runway_loop video",
-  { transform: "rotate3d(30, 1, 0, 50deg) scale(0.5)", opacity: "0" },
-  {
-    transform: "rotate3d(1, 1, 1, 0deg)",
-    opacity: "1",
-    scrollTrigger: {
-      trigger: ".runway_loop video",
-      start: "0 100%",
-      end: "+=500",
-      scrub: 3,
-      // markers: true,
-    },
-  }
-);
 ScrollTrigger.matchMedia({
   "(min-width: 800px)": function () {
     gsap.fromTo(
