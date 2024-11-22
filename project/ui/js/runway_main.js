c182e3241786abdeaa15c52e2796ef3cc49bc61e
jm7651 <jmp7651@gmail.com>
2024-11-23 01:51:47 +0900

    commit

--- project/ui/js/runway_main.js
+++ project/ui/js/runway_main.js
@@ -1,229 +1,235 @@
-AOS.init();
-
-// HTML에서 이미지 태그의 id가 "myImage"인 이미지를 참조합니다.
-var image = document.querySelector(".gradient_bg");
-
-// 모바일 디바이스인지 여부를 확인합니다.
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  // 모바일 환경에서 이미지를 변경합니다.
-  image.classList.add("mobile"); // 새로운 이미지의 경로를 지정합니다.
-} else {
-  // 모바일이 아닌 환경에서 이미지를 변경합니다.
-  image.classList.remove("mobile"); // 기존 이미지의 경로를 지정합니다.
-}
-
-// const updateBackgroundLayout = function () {
-//   const gradientBg = document.querySelector(".gradient_bg");
-//   if (window.innerWidth < 1000) {
-//     gradientBg.classList.add("mobile");
-//   } else {
-//     gradientBg.classList.remove("mobile");
-//   }
-// };
-// console.log(window.innerWidth);
-
-// const mq = window.matchMedia("(max-width: 1000px)");
-// mq.addEventListener(updateBackgroundLayout);
-// updateBackgroundLayout();
-
-// const runwayVideo002 = document.querySelector(".runway_002_video");
-// console.log(runwayVideo002);
-// runwayVideo002.pause();
-// const runwayvideo002player = new IntersectionObserver(
-//   (entries) => {
-//     entries.forEach((entry) => {
-//       entry.target.classList.toggle("visible", entry.isIntersecting);
-
-//       // 1. 요소가 화면에 나타났다면
-//       if (entry.isIntersecting) {
-//         // 2. 옵저버 객체의 unobserve 메서드로 요소의 감지를 해제합니다!
-//         entry.target.play();
-//       } else {
-//         entry.target.pause();
-//       }
-//     });
-//   },
-//   { threshold: 0.5 }
-// );
-// runwayvideo002player.observe(runwayVideo002);
-
-const linkVideo = document.querySelector(".link_video");
-const runwayVideo = document.querySelector(".runway_video");
-const runway3Video = document.querySelector(".runway_003_video video");
-const runwayvideo001player = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      entry.target.classList.toggle("visible", entry.isIntersecting);
-
-      // 1. 요소가 화면에 나타났다면
-      if (entry.isIntersecting) {
-        // 2. 옵저버 객체의 unobserve 메서드로 요소의 감지를 해제합니다!
-        entry.target.play();
-      } else {
-        entry.target.pause();
+// 유틸리티 함수
+const utils = {
+  throttle(func, limit) {
+    let inThrottle;
+    return function(...args) {
+      if (!inThrottle) {
+        func.apply(this, args);
+        inThrottle = true;
+        setTimeout(() => inThrottle = false, limit);
       }
+    };
+  },
+
+  isTouchDevice() {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  },
+
+  isDeviceMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  }
+};
+
+// 디바이스 체크 및 설정
+const deviceManager = {
+  init() {
+    const gradientBg = document.querySelector(".gradient_bg");
+    if (gradientBg) {
+      if (utils.isDeviceMobile()) {
+        gradientBg.classList.add("mobile");
+      } else {
+        gradientBg.classList.remove("mobile");
+      }
+    }
+
+    if (utils.isTouchDevice()) {
+      document.body.classList.add('touch-device');
+    }
+  }
+};
+
+// 비디오 플레이어 관리
+const videoManager = {
+  videos: {},
+
+  init() {
+    this.videos = {
+      linkVideo: document.querySelector(".link_video"),
+      runwayVideo: document.querySelector(".runway_video"),
+      runway3Video: document.querySelector(".runway_003_video video")
+    };
+
+    if (!this.videos.linkVideo || !this.videos.runwayVideo) return;
+
+    this.setupVideoObserver();
+    this.setupVideoEventListeners();
+    this.startInitialVideo();
+  },
+
+  setupVideoObserver() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const video = entry.target;
+          if (entry.isIntersecting) {
+            video.play();
+          } else {
+            video.pause();
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    [this.videos.linkVideo, this.videos.runway3Video].forEach(video => {
+      if (video) observer.observe(video);
     });
   },
-  { threshold: 0.5 }
-);
 
-runwayvideo001player.observe(linkVideo);
-runwayvideo001player.observe(runway3Video);
-function video_start() {
-  linkVideo.play();
-  runwayVideo.pause();
-}
-function video_start2() {
-  runwayVideo.play();
-  linkVideo.pause();
-}
+  setupVideoEventListeners() {
+    this.videos.linkVideo.addEventListener("ended", () => this.playRunwayVideo());
+    this.videos.runwayVideo.addEventListener("ended", () => this.playLinkVideo());
+  },
 
-linkVideo.addEventListener("ended", myHandler, false);
-function myHandler(e) {
-  video_start2();
-}
-runwayVideo.addEventListener("ended", myHandler2, false);
-function myHandler2(e) {
-  video_start();
-}
-video_start();
+  playLinkVideo() {
+    this.videos.linkVideo.play();
+    this.videos.runwayVideo.pause();
+  },
 
-const tabSlideBtn = document.querySelectorAll(".tab_slide_button");
+  playRunwayVideo() {
+    this.videos.runwayVideo.play();
+    this.videos.linkVideo.pause();
+  },
 
-tabSlideBtn.forEach(function (item) {
-  item.addEventListener("click", function () {
-    tabSlideBtn.forEach(function (e) {
-      e.classList.remove("active");
-    });
-    item.classList.add("active");
-  });
-});
-//프리뷰
-function tabUi(evt, tabname) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("tab_text");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabname).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-function iframe01() {
-  document.getElementById("getFrame").src =
-    "https://app.storylane.io/demo/qapzdlq8fxvx";
-}
-function iframe02() {
-  document.getElementById("getFrame").src =
-    "https://app.storylane.io/demo/z17lzthjencd";
-}
-function iframe03() {
-  document.getElementById("getFrame").src =
-    "https://app.storylane.io/demo/vgeijgj1xwnr";
-}
-function iframe04() {
-  document.getElementById("getFrame").src =
-    "https://app.storylane.io/demo/ycb1ybyfzryz";
-}
-function iframe04() {
-  if (document.querySelector("body.ko")) {
-    document.getElementById("getFrame").src =
-      "https://app.storylane.io/demo/ycb1ybyfzryz";
-  } else {
-    document.getElementById("getFrame").src =
-      "https://app.storylane.io/demo/fpxlpixxp9cn";
-  }
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
-const overlay = document.querySelectorAll(".md_overlay");
-const closeButton = document.querySelector(".modal_close");
-const loadbg = document.querySelector(".loadbg");
-const defaultIframe = document.querySelector("#defaultIframe");
-const modal = document.querySelectorAll(".modal");
-const youtubeButton = document.getElementById("runway_youtube_open");
-const openYoutubeModal = () => {
-  defaultModal.classList.add("youtube_modal");
-  defaultModal.classList.remove("hidden");
-  document.body.style.overflowY = "hidden";
-};
-const closeModal = () => {
-  document.body.style.overflowY = "unset";
-  for (let i = 0; i < modal.length; i++) {
-    defaultModal.classList.remove("youtube_modal");
-    modal[i].classList.add("hidden");
+  startInitialVideo() {
+    this.playLinkVideo();
   }
 };
-youtubeButton.addEventListener("click", openYoutubeModal);
-closeButton.addEventListener("click", closeModal);
-for (let i = 0; i < overlay.length; i++) {
-  overlay[i].addEventListener("click", closeModal);
-}
 
-document.addEventListener("DOMContentLoaded", function () {
-  var parallaxText = document.querySelector(".runwaypage_maintext");
-  window.addEventListener("scroll", function () {
-    var scrollPos = window.pageYOffset;
-    if (window.innerWidth > 451) {
-      parallaxText.style.transform = "translate(0, -" + scrollPos / 1.2 + "px)";
-    } else {
-      parallaxText.style.transform = "translate(0, -" + scrollPos / 3.5 + "px)";
+// 스크롤 효과 관리
+const scrollEffectsManager = {
+  elements: {},
+  config: {
+    fps: 16 // 60fps에 해당하는 간격
+  },
+
+  init() {
+    this.elements = {
+      parallaxText: document.querySelector(".runwaypage_maintext"),
+      textWrapper: document.querySelector(".runway_pin_text_wrapper"),
+      listItems: document.querySelectorAll(".list-item")
+    };
+
+    if (!this.elements.parallaxText || !this.elements.textWrapper || !this.elements.listItems.length) {
+      return;
     }
-  });
-});
-const textWrapperTop = document.querySelector(
-  ".runway_pin_text_wrapper"
-).offsetTop;
-const windowHeight = innerHeight;
-const textWrapperHeight = document.querySelector(
-  ".runway_pin_text_wrapper"
-).offsetHeight;
-const textWrapperHeightHarf = textWrapperHeight / 2;
 
-const listStyleChangeStartY = windowHeight - textWrapperHeight;
-const listStyleChangeEndY = textWrapperTop + textWrapperHeightHarf;
+    this.setupScrollMetrics();
+    this.bindScrollEvents();
+  },
 
-const listItems = document.querySelectorAll(".list-item");
-const division =
-  (listStyleChangeEndY - listStyleChangeStartY) / listItems.length;
-window.addEventListener("scroll", () => {
-  if (document.getElementById("on"))
-    document.getElementById("on").removeAttribute("id");
-  if (
-    window.scrollY > listStyleChangeStartY &&
-    window.scrollY < listStyleChangeEndY
-  ) {
-    const targetIndex = Math.round(
-      (window.scrollY - listStyleChangeStartY) / division
-    );
-    if (listItems[targetIndex]) listItems[targetIndex].id = "on";
+  setupScrollMetrics() {
+    const { textWrapper } = this.elements;
+    this.metrics = {
+      textWrapperTop: textWrapper.offsetTop,
+      windowHeight: window.innerHeight,
+      textWrapperHeight: textWrapper.offsetHeight,
+      textWrapperHeightHalf: textWrapper.offsetHeight / 2,
+    };
+
+    this.metrics.listStyleChangeStartY = this.metrics.windowHeight - this.metrics.textWrapperHeight;
+    this.metrics.listStyleChangeEndY = this.metrics.textWrapperTop + this.metrics.textWrapperHeightHalf;
+    this.metrics.division = (this.metrics.listStyleChangeEndY - this.metrics.listStyleChangeStartY) / this.elements.listItems.length;
+  },
+
+  bindScrollEvents() {
+    const handleParallax = utils.throttle(() => {
+      const scrollPos = window.pageYOffset;
+      requestAnimationFrame(() => {
+        const factor = window.innerWidth > 451 ? 1.2 : 3.5;
+        this.elements.parallaxText.style.transform = `translate(0, -${scrollPos / factor}px)`;
+      });
+    }, this.config.fps);
+
+    const handleListItems = utils.throttle(() => {
+      const prevOn = document.getElementById("on");
+      if (prevOn) prevOn.removeAttribute("id");
+
+      const scrollY = window.scrollY;
+      if (scrollY > this.metrics.listStyleChangeStartY && scrollY < this.metrics.listStyleChangeEndY) {
+        const targetIndex = Math.round((scrollY - this.metrics.listStyleChangeStartY) / this.metrics.division);
+        requestAnimationFrame(() => {
+          if (this.elements.listItems[targetIndex]) {
+            this.elements.listItems[targetIndex].id = "on";
+          }
+        });
+      }
+    }, this.config.fps);
+
+    window.addEventListener("scroll", handleParallax, { passive: true });
+    window.addEventListener("scroll", handleListItems, { passive: true });
   }
-});
-
-//히스토리
-history.pushState(null, null, location.href);
-window.onpopstate = function () {
-  history.go(1);
 };
 
-//로딩
-window.addEventListener("load", function () {
-  document.body.classList.remove("no-scroll");
-  var blackblock = document.querySelector(".blackblock");
-  if (blackblock) {
-    blackblock.style.transition = "opacity 1s";
-    blackblock.style.opacity = "0";
-    setTimeout(function () {
-      blackblock.style.display = "none";
-    }, 1000);
+// 모달 관리
+const modalManager = {
+  elements: {},
+
+  init() {
+    this.elements = {
+      overlay: document.querySelectorAll(".md_overlay"),
+      closeButton: document.querySelector(".modal_close"),
+      modals: document.querySelectorAll(".modal"),
+      youtubeButton: document.getElementById("runway_youtube_open")
+    };
+
+    if (!this.elements.youtubeButton) return;
+
+    this.bindEvents();
+  },
+
+  bindEvents() {
+    this.elements.youtubeButton.addEventListener("click", () => this.openYoutubeModal());
+    this.elements.closeButton?.addEventListener("click", () => this.closeModal());
+    this.elements.overlay.forEach(overlay => {
+      overlay.addEventListener("click", () => this.closeModal());
+    });
+  },
+
+  openYoutubeModal() {
+    defaultModal.classList.add("youtube_modal");
+    defaultModal.classList.remove("hidden");
+    document.body.style.overflowY = "hidden";
+  },
+
+  closeModal() {
+    document.body.style.overflowY = "unset";
+    this.elements.modals.forEach(modal => {
+      defaultModal.classList.remove("youtube_modal");
+      modal.classList.add("hidden");
+    });
   }
+};
+
+// 페이지 로딩 관리
+const pageLoadManager = {
+  init() {
+    window.addEventListener("load", () => {
+      document.body.classList.remove("no-scroll");
+      const blackblock = document.querySelector(".blackblock");
+      if (blackblock) {
+        blackblock.style.transition = "opacity 1s";
+        blackblock.style.opacity = "0";
+        setTimeout(() => {
+          blackblock.style.display = "none";
+        }, 1000);
+      }
+    });
+
+    // 히스토리 관리
+    history.pushState(null, null, location.href);
+    window.onpopstate = () => history.go(1);
+  }
+};
+
+// 앱 초기화
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
+  deviceManager.init();
+  videoManager.init();
+  scrollEffectsManager.init();
+  modalManager.init();
+  pageLoadManager.init();
 });
--- project/ui/js/summary_modal.js
+++ project/ui/js/summary_modal.js
@@ -2,12 +2,12 @@ const summaryModal = document.querySelector(".summary_modal");
 const btnModal = document.querySelector(".summary_button");
 // const modalClose = window.parent.document.querySelector(".modal_close");
 const summaryClose = document.querySelector(".summary_close");
-btnModal.addEventListener("click", (e) => {
-  console.log("hi");
-  summaryModal.style.display = "flex";
-  document.body.classList.add("stop-scroll");
-  // modalClose.style.display = "none";
-});
+// btnModal.addEventListener("click", (e) => {
+//   console.log("hi");
+//   summaryModal.style.display = "flex";
+//   document.body.classList.add("stop-scroll");
+//   // modalClose.style.display = "none";
+// });
 summaryClose.addEventListener("click", (e) => {
   summaryModal.style.display = "none";
   document.body.classList.remove("stop-scroll");
